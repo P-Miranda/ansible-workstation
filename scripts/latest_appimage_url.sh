969ea3a43fbcd669cb99e14d29d6d09e2d5924a1
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2022-07-26 12:53:19 +0100

    feat(script): get url for latest appimage release - add script to obtain url for latest appimage release for a github   repository - this script can be used to set the download url as a ansible variable

---
+++ scripts/latest_appimage_url.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Get download link for latest AppImage in github release
+# Usage: latest_appimage_url.sh organization/user repository
+
+if [ $# -lt 2 ]
+then
+    echo "Usage: latest_appimage_url.sh [organization] [repository]"
+    exit 1
+fi
+
+organization=$1
+repository=$2
+
+github_api="https://api.github.com/repos/$organization/$repository/releases/latest"
+
+download_link=$(curl -s $github_api \
+    | grep "browser_download_url" \
+    | grep "AppImage\"\$" \
+    | awk {'print $2'})
+
+echo $download_link
